139a04f32b4926c6f92e1f067dfec65c64153b64
Dane <sodanae23@gmail.com>
2026-08-27 08:32:11 +0700

    feat: register orderitem model inside admin.py to display table on backend dashboard

--- web_app/admin_dashboard/admin.py
+++ web_app/admin_dashboard/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Book, Order, Department  # Imports your database tables
+from .models import Book, Order, Department, OrderItem  # Imports database tables
 
-# 🎯 CRITICAL SYSTEM REGISTRATION: This tells Django to print your bookstore tables on your admin site layout!
+# CRITICAL SYSTEM REGISTRATION
 admin.site.register(Book)
 admin.site.register(Order)
 admin.site.register(Department)
+admin.site.register(OrderItem)
